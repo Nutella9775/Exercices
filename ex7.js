c822dcf395eee7e79d55a3c147504d212e59804d
Nutella9775 <adrienbarbet7@gmail.com>
2025-09-15 15:44:26 +0200

    feat if free livraison

---
+++ ex7.js
@@ -0,0 +1,10 @@
+const montant_min = 100;
+
+let montant_total = 500;
+
+if (montant >= montant_min) {
+    console.log("Livraison gratuite");
+}
+else {
+    console.log("Frais de livraison : 10 euros");
+}
