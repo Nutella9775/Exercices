5a8c214f37867b1df6462bee7838d251a068375e
Nutella9775 <adrienbarbet7@gmail.com>
2025-09-15 14:56:41 +0200

    initial commit

---
+++ ex1.js
@@ -0,0 +1,6 @@
+let celsius = 20;
+
+let fahrenheit = (celsius * 9/5) + 32;
+
+console.log("Température en Celsius : " + celsius + " °C");
+console.log("Température en Fahrenheit : " + fahrenheit + " °F");
